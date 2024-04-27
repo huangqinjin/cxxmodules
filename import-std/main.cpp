5dd9daf5505ebe69dfa55a0585194795b6745219
huangqinjin <huangqinjin@gmail.com>
2024-04-27 22:29:33 +0800

    clang import std
    https://github.com/llvm/llvm-project/pull/75741

--- import-std/main.cpp
+++ import-std/main.cpp
@@ -1,6 +1,17 @@
+#include <version>
+
+// https://github.com/llvm/llvm-project/issues/89579
+#ifdef _LIBCPP_VERSION
+#define __cpp_lib_modules 202207L
+#endif
+
+#ifdef __cpp_lib_modules
 import std;
+#endif
 
 int main()
 {
+#ifdef __cpp_lib_modules
     std::println("{}", "Hello, World!");
+#endif
 }
