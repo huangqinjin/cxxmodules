60f272993edd5ec7b30f3c111c0921aa9dcba8b1
huangqinjin <huangqinjin@gmail.com>
2024-05-08 22:09:21 +0800

    clang-cl import std

--- import-std/main.cpp
+++ import-std/main.cpp
@@ -5,6 +5,10 @@
 #define __cpp_lib_modules 202207L
 #endif
 
+#if defined(__clang__) && defined(_MSC_VER)
+#define __cpp_lib_modules 202207L
+#endif
+
 #ifdef __cpp_lib_modules
 import std;
 #endif
---
+++ import-std/std.cppm
@@ -0,0 +1,1 @@
+#include <modules/std.ixx>
