39885a99aa6b41859c49a4d6a25ffd76f8cfeb64
huangqinjin <huangqinjin@gmail.com>
2024-06-10 19:07:11 +0800

    update to clang 19
    1. __cpp_lib_modules now is defined.
    90091
    
    2. clang-cl supports -x option for all languages.
    89772
    
    3. Header units can import static members.
    98309
    non-inline external definitions are not permitted in C++ header units
    
    4. -Xmicrosoft-windows-sys-root
     94731
    
    5. clang-cl gained options for compiling C++ modules.
    98761
    
    6. fix unused argument warning for /std:c++20 when compiling c++ modules.
    99300

--- import-std/main.cpp
+++ import-std/main.cpp
@@ -1,10 +1,5 @@
 #include <version>
 
-// https://github.com/llvm/llvm-project/issues/89579
-#ifdef _LIBCPP_VERSION
-#define __cpp_lib_modules 202207L
-#endif
-
 #ifdef __cpp_lib_modules
 import std;
 #endif
