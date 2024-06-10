693f949ebe59642a73725b37a91035cac1a458bd
huangqinjin <huangqinjin@gmail.com>
2024-06-10 19:07:11 +0800

    update to clang 19
    1. __cpp_lib_modules now is defined.
    90091
    
    2. clang-cl supports -x option for all languages.
    89772
    
    3. Header units can import static members.
    98309
    non-inline external definitions are not permitted in C++ header units
    
    4. -Xmicrosoft-windows-sys-root
     94731

--- header-unit/main.cpp
+++ header-unit/main.cpp
@@ -6,18 +6,9 @@ import "hello.hpp";
 #error "Macros must be imported from header units"
 #endif
 
-#if !defined(__clang__) || __has_include("iostream.pcm")
 import <iostream>;
 
 int main()
 {
     std::cout << hello() << std::endl;
 }
-#else
-#include <stdio.h>
-
-int main()
-{
-    printf("NO iostream header unit: %s\n", hello());
-}
-#endif
